2067463fb8483f0d256b2654f4a683d5363c13ec
Ewa <ewa.rosset@softwire.com>
2022-02-03 13:44:59 +0000

    test hotjar

--- src/App.js
+++ src/App.js
@@ -4,16 +4,31 @@ import './App.css';
 import {PhotoViewer} from "./PhotoViewer/PhotoViewer";
 import {Clicker} from "./Clicker/Clicker";
 import {imageUrls} from "./PhotoViewer/getImages"
-import React from "react";
+import React, {useEffect} from "react";
+import { hotjar } from 'react-hotjar';
 
+
+const hotjarTest = () => {
+    hotjar.identify('USER_ID', { userProperty: 'value' });
+    hotjar.event('test-button');
+    hotjar.stateChange('/my/page');
+};
 
 function App() {
+    
+    useEffect(() => {
+        hotjar.initialize(2813391, 6);
+    }, []);
     return (
         <div className="App">
             <div>
                 <h1 className="websiteHeader">React Photo Viewer</h1>
                 <PhotoViewer/>
                 <Clicker/>
+                <button
+                    type="button"
+                    onClick={hotjarTest}
+                />
             </div>
         </div>
 
