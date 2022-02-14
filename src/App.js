b1485c9839c1f6a73dcf472528a03c7572f9858a
Ewa <ewa.rosset@softwire.com>
2022-02-14 14:36:45 +0000

    test

--- src/App.js
+++ src/App.js
@@ -2,32 +2,30 @@
 import './App.css';
 import React, {useEffect} from "react";
 import { hotjar } from 'react-hotjar';
-import {HashRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import {Route, Routes, useLocation } from 'react-router-dom';
 import Homepage from "./Pages/Homepage/Homepage";
 import TestPage from "./Pages/Page2/TestPage";
 import ReactGA from 'react-ga';
 
+
 function usePageViews() {
-    let location = useLocation();
+    const location = useLocation();
     useEffect(() => {
-        // ReactGA.initialize('UA-219300525-2');
-        // ReactGA.set({ page: location.pathname });
-        // ReactGA.pageview(location.pathname);
-        ReactGA.pageview(window.location.pathname + window.location.search);
+        ReactGA.initialize('UA-219300525-2');
+        ReactGA.pageview(location.pathname + location.search);
     }, [location]);
 }
-
 
 function App() {
 
     // ReactGA.initialize('UA-219300525-2');
 
 
-    useEffect(() => {
-        hotjar.initialize(2813391, 6);
-    }, []);
-
-    usePageViews();
+    // useEffect(() => {
+    //     hotjar.initialize(2813391, 6);
+    // }, []);
+    //
+    // usePageViews();
 
     return (
         <Routes>
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {HashRouter} from "react-router-dom";
+import {HashRouter, useLocation} from "react-router-dom";
 import {Helmet, HelmetProvider} from "react-helmet-async";
 
 const hotJar = `(function(h,o,t,j,a,r){
@@ -14,12 +14,20 @@ const hotJar = `(function(h,o,t,j,a,r){
                   r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
                   a.appendChild(r);
               })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`;
-
+//
 const googleAnalytics = `window.dataLayer = window.dataLayer || [];
                   function gtag(){dataLayer.push(arguments);}
                   gtag('js', new Date());
 
                   gtag('config', 'UA-219300525-2');`
+
+// const history = createBrowserHistory();
+//
+// history.listen((location) => {
+//     if (window.ga) {
+//         window.ga('send', 'pageview', location.pathname);
+//     }
+// });
 
 ReactDOM.render(
   <React.StrictMode>
@@ -28,10 +36,10 @@ ReactDOM.render(
               <script type="text/javascript" charSet="UTF-8"
                       src="//cdn.cookie-script.com/s/734d039edba9d0ec7bfa6cf3e7f8d521.js">
               </script>
-              <script async src="https://www.googletagmanager.com/gtag/js?id=UA-219300525-2"></script>
-              <script>
-                  {googleAnalytics}
-              </script>
+              {/*<script async src="https://www.googletagmanager.com/gtag/js?id=UA-219300525-2"></script>*/}
+              {/*<script>*/}
+              {/*    {googleAnalytics}*/}
+              {/*</script>*/}
               <script>
                   {hotJar}
               </script>
